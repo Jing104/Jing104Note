c65255c65b2f32f993c4901627818c4cbbe96d3b
Jing104 <2046485376@qq.com>
2025-04-23 18:54:00 +0800

    修改base为默认/

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/Jing104Note/",
+  base: "/",
 
   lang: "zh-CN",
   title: "Jing104-Note",
